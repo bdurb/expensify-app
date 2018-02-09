adfd675d59650351085a9a42e39fd3f3e18db297
bdurb <brian@rpmgreenville.com>
2018-02-09 12:12:52 -0500

    added imports to app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const store = configureStore();
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
