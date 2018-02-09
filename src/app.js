c12e7ff25fde6acfc6e2e9fcb212eb1131b85e7b
bdurb <brian@rpmgreenville.com>
2018-02-09 15:55:42 -0500

    imported provider

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -10,4 +11,10 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+  <Provider store ={store}>
+    <AppRouter />
+  </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'));
