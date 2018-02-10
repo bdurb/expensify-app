eecf08999dced98960b6d863f694fd3cfa5e0da7
bdurb <brian@rpmgreenville.com>
2018-02-10 17:21:09 -0500

    set up date and amount filtering

--- src/app.js
+++ src/app.js
@@ -11,13 +11,9 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(setTextFilter('water'));
+store.dispatch(addExpense({ description: 'Water bill', amount: 2500 }));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 45000 }));
 
-// setTimeout(() => {
-//   store.dispatch(setTextFilter('bill'));
-// }, 3000)
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
