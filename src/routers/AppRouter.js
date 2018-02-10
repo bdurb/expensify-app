160751e287e52feb44b57c00b4d2c793cf144338
bdurb <brian@rpmgreenville.com>
2018-02-10 16:23:44 -0500

    made changes to router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
-import AddExpense from '../components/AddExpense';
-import EditExpense from '../components/EditExpense';
-import ExpenseDashboard from '../components/ExpenseDashboard';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import Header from '../components/Header';
-import Help from '../components/Help';
-import NotFound from '../components/NotFound';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
 
 
 const AppRouter = () => (
@@ -13,11 +13,11 @@ const AppRouter = () => (
     <div>
       <Header />
         <Switch>
-          <Route path="/" component={ExpenseDashboard} exact={true} />
-          <Route path="/create" component={AddExpense} />
-          <Route path="/edit/:id" component={EditExpense} />
-          <Route path="/help" component={Help} />
-          <Route component={NotFound} />
+          <Route path="/" component={ExpenseDashboardPage} exact={true} />
+          <Route path="/create" component={AddExpensePage} />
+          <Route path="/edit/:id" component={EditExpensePage} />
+          <Route path="/help" component={HelpPage} />
+          <Route component={NotFoundPage} />
         </Switch>
     </div>
   </BrowserRouter>
