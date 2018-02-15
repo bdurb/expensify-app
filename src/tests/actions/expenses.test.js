66ff5d23f0f95f51174c6fb09909bb4a16fea495
bdurb <brian@rpmgreenville.com>
2018-02-15 12:49:13 -0500

    set up test for edit expesne action

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,1 +1,20 @@
 import {addExpense, editExpense, removeExpense } from '../../actions/expenses';
+
+test('should setup remove expsense action object', () => {
+  const action = removeExpense( { id: '123abc'});
+  expect(action).toEqual({
+    type: 'REMOVE_EXPENSE',
+    id: '123abc'
+  })
+});
+
+test('should edit an expense action object', () => {
+  const action = editExpense('123', { note: 'Hello' });
+  expect(action).toEqual({
+    type: 'EDIT_EXPENSE',
+    id: '123',
+    updates: {
+      note: 'Hello'
+    }
+  });
+});
