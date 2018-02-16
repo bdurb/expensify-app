5e60f0c3d8323e5d62fc5e0d0922f69a74b945f2
bdurb <brian@rpmgreenville.com>
2018-02-16 17:48:26 -0500

    set up tests for expenses action

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -18,3 +18,34 @@ test('should edit an expense action object', () => {
     }
   });
 });
+
+test('should set up add expense action object with provided values',() => {
+  const expenseData = {
+    description: 'Rent',
+    amount: 109500,
+    createdAt: 1000,
+    note: 'Last Months Rent'
+  }
+  const action = addExpense(expenseData);
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      ...expenseData,
+      id: expect.any(String)
+    }
+  })
+});
+
+test('should setup add expense action object with default values', () => {
+  const action = addExpense();
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+      expense: {
+        description: '',
+        note: '',
+        amount: 0,
+        createdAt: 0,
+        id: expect.any(String)
+      }
+  })
+});
