4cfb244db9cdfddc1b6c9d62827c5eb83c43fd98
bdurb <brian@rpmgreenville.com>
2018-02-20 21:11:48 -0500

    setting up edit expense page tests

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -2,3 +2,31 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import expenses from '../fixtures/expenses';
 import { EditExpensePage } from '../../components/EditExpensePage';
+
+let editExpense, removeExpense, history, wrapper;
+
+beforeEach(() => {
+  editExpense = jest.fn();
+  removeExpense = jest.fn();
+  history = { push: jest.fn() };
+  wrapper = shallow(
+    <EditExpensePage
+      editExpense={editExpense}
+      removeExpense={removeExpense}
+      history={history}
+      expense={expenses[2]}
+    />
+  );
+});
+
+test('should render EditExpensePage', () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('', () => {
+
+});
+
+test('', () => {
+
+});
