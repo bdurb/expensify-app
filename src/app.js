c9dc778ce62dbb1f63e03fba11a957843b5fc8aa
bdurb <brian@rpmgreenville.com>
2018-02-22 23:05:55 -0500

    took out dummy data from app.js

--- src/app.js
+++ src/app.js
@@ -12,13 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 2500 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 45000 }));
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
