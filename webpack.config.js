e4b1342c67be7f15b199d48861457a00d661ceb4
bdurb <brian@rpmgreenville.com>
2018-02-24 19:04:44 -0500

    setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,18 @@ module.exports = (env) => {
         test: /\.s?css$/,
         use: CSSExtract.extract({
           use: [
-            'css-loader',
-            'sass-loader'
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
           ]
         })
       }]
@@ -29,7 +39,7 @@ module.exports = (env) => {
     plugins: [
       CSSExtract
     ],
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true
