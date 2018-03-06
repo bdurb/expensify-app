42c925f258825ecd160a36e38db13577f31d6cec
bdurb <brian@rpmgreenville.com>
2018-03-06 17:54:41 -0500

    set up Public Route for login page

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,6 +7,7 @@ import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
@@ -14,7 +15,7 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
         <Switch>
-          <Route path="/" component={LoginPage} exact={true} />
+          <PublicRoute path="/" component={LoginPage} exact={true} />
           <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
           <PrivateRoute path="/create" component={AddExpensePage} />
           <PrivateRoute path="/edit/:id" component={EditExpensePage} />
