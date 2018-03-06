a504aa4b01efaa62c4a1bf741d06ddcfddf3d6ef
bdurb <brian@rpmgreenville.com>
2018-03-06 17:50:21 -0500

    removed help page routes and component

--- src/components/HelpPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HelpPage = () => (
-  <div>
-    Get Help Here!
-  </div>
-);
-
-export default HelpPage;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,7 +4,6 @@ import createHistory from 'history/createBrowserHistory';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
-import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage';
 import PrivateRoute from './PrivateRoute';
@@ -19,7 +18,6 @@ const AppRouter = () => (
           <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
           <PrivateRoute path="/create" component={AddExpensePage} />
           <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-          <Route path="/help" component={HelpPage} />
           <Route component={NotFoundPage} />
         </Switch>
     </div>
