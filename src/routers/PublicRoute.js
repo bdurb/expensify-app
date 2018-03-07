45a3ce73e6e341c0974120bd1778a7e9c7291a28
bdurb <brian@rpmgreenville.com>
2018-03-06 19:39:42 -0500

    removed header component from public route

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
-import Header from '../components/Header';
 
 export const PublicRoute = ({
   isAuthenticated,
@@ -10,10 +9,7 @@ export const PublicRoute = ({
 }) => (
   <Route {...rest} component={(props) => (
     !isAuthenticated ? (
-      <div>
-        <Header />  
         <Component {...props} />
-      </div>
     ) : (
       <Redirect to='/dashboard' />
     )
